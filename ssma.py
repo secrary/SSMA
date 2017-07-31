7f4782b13f526ee327d6f7b9651bab886c7953b5
pielco11 <francesco.poldi@protonmail.ch>
2017-07-31 10:11:10 +0200

    fix elasticsearch

--- ssma.py
+++ ssma.py
@@ -27,7 +27,7 @@ from src.report import pe_report, elf_report, others_report
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Simple Static Malware Analyzer")
-    parser.add_argument("filename", help="/path/to/file")
+    parser.add_argument("-f", "--filename", help="/path/to/file")
     parser.add_argument("-k", "--api-key", help="Virustotal API key")
     parser.add_argument("-d", "--document", help="check document/MS Office file", action="store_true")
     parser.add_argument("-F", "--Flush", help="Flush output, no interrupt (on/off)")
@@ -35,6 +35,7 @@ if __name__ == '__main__':
     parser.add_argument("-y", "--yara", help="Scan file with your Yara-Rule")
     parser.add_argument("-D", "--directory", help="Mass analysis from a dir (/path/)")
     parser.add_argument("-r", "--report", help="Generate json format report (yes/no/elasticsearch)")
+    parser.add_argument("-t", "--table", help="Markdown output")
 
     args = parser.parse_args()
 
