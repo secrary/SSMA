d596bed7fe269ac8e7f323e0cc83c0fb697e161c
delirious-lettuce <delirious.lettuce@gmail.com>
2017-12-19 22:16:06 -0700

    Simplify `mass_analysis`   * Rename `dir` to `target_dir` since it shadows a built-in function   * Remove unnecessary `os.listdir` call   * Use `os.makedirs` instead of `os.mkdir` (`os.path.exists` unneeded)   * Remove excess space character from `os.system` call

--- src/mass_analysis.py
+++ src/mass_analysis.py
@@ -7,14 +7,9 @@ import os
 
 
 def start_scan(args):
-    dir = os.path.abspath(args.directory)
-    list = os.listdir(dir)
-    if list:
-        for root, _, filenames in os.walk(dir):
-            for file in filenames:
-                filename = os.path.join(root, file)
-
-                if not os.path.exists("analysis_report"):
-                    os.mkdir("analysis_report")
-
-                os.system("python3 ssma.py  %s -r yes" % filename)
+    target_dir = os.path.abspath(args.directory)
+    for root, _, filenames in os.walk(target_dir):
+        for file in filenames:
+            os.makedirs("analysis_report", exist_ok=True)
+            filename = os.path.join(root, file)
+            os.system("python3 ssma.py %s -r yes" % filename)
