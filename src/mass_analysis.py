c4919e8bfa5c0a5d80d82cb4eddc1cf1ea70de79
delirious-lettuce <delirious.lettuce@gmail.com>
2017-12-19 22:31:58 -0700

    Rename `file` to `filename` since it shadows a built-in function

--- src/mass_analysis.py
+++ src/mass_analysis.py
@@ -9,7 +9,7 @@ import os
 def start_scan(args):
     target_dir = os.path.abspath(args.directory)
     for root, _, filenames in os.walk(target_dir):
-        for file in filenames:
+        for filename in filenames:
             os.makedirs("analysis_report", exist_ok=True)
-            filename = os.path.join(root, file)
-            os.system("python3 ssma.py %s -r yes" % filename)
+            file_path = os.path.join(root, filename)
+            os.system("python3 ssma.py %s -r yes" % file_path)
